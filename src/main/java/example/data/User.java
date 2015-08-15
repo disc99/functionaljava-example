7d0bf81cfbf8e3b87413a79944d647d9f35c3da0
d.kanehira <disc99.o@gmail.com>
2015-08-15 13:07:24 +0900

    Add sample method

--- src/main/java/example/data/User.java
+++ src/main/java/example/data/User.java
@@ -1,11 +1,13 @@
 package example.data;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /**
  * Sample User Bean
  */
 @AllArgsConstructor
+@Data
 public class User {
     public int age;
     public String name;
